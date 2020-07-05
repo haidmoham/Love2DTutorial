6ad389af2e1d679006774f8fc96224612b0fde82
Mohammad Haider <haidmoham@gmail.com>
2020-07-05 00:05:53 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Basic Game made with Love2D using this tutorial
+http://www.osmstudios.com/tutorials/your-first-love2d-game-in-200-lines-part-1-of-3
---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- Config 
+function love.conf(t)
+    t.titile = "Love2D Tutorial Shooter"
+    t.version = "11.3"
+    t.window.width = 480
+    t.window.height = 800
+
+    -- Windows Debugging
+    t.console = false
+end
